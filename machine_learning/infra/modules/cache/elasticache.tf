e04e0adc0b024fb50d73201dd7a749df0b446747
Réda Maizate <reda.maizate@affluences.com>
2023-06-17 13:08:58 +0200

    feat: added cache (redis) module

---
+++ machine_learning/infra/modules/cache/elasticache.tf
@@ -0,0 +1,18 @@
+resource "aws_elasticache_subnet_group" "group" {
+  name       = local.elasticache_subnet_group_name
+  subnet_ids = [var.subnet_id]  # Replace with your subnet IDs
+}
+
+resource "aws_elasticache_cluster" "redis-cluster" {
+  cluster_id               = local.elasticache_cluster_name
+  engine                   = "redis"
+  engine_version           = "6.x"
+  node_type                = "cache.t2.micro"
+  num_cache_nodes          = 1
+  port                     = 6379
+  subnet_group_name        = aws_elasticache_subnet_group.group.name
+  parameter_group_name     = "default.redis6.x"
+  maintenance_window       = "mon:03:00-mon:04:00"  # Maintenance window in UTC
+  snapshot_retention_limit = 5
+  snapshot_window          = "08:00-10:00"  # Snapshot window in UTC
+}
---
+++ machine_learning/infra/modules/cache/variables.tf
@@ -0,0 +1,24 @@
+locals {
+  elasticache_subnet_group_name = "${var.app_name}-elasticache-subnet-group"
+  elasticache_cluster_name      = "${var.app_name}-elasticache-cluster"
+}
+
+variable "app_name" {
+  type    = string
+  default = ""
+}
+
+variable "subnet_id" {
+  type    = string
+  default = ""
+}
+
+variable "vpc_id" {
+  type    = string
+  default = ""
+}
+
+#variable "key_name" {
+#  type    = string
+#  default = ""
+#}
