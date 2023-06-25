2bcd7919c1d0ce38be1abed8ca04e668b02b5a0d
WxMAMADIANxW <49515789+WxMAMADIANxW@users.noreply.github.com>
2023-06-25 13:36:49 +0200

    project list

--- aiditor_web/lib/pages/home_page.dart
+++ aiditor_web/lib/pages/home_page.dart
@@ -1,27 +1,215 @@
+import 'dart:convert';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:http/http.dart' as http;
 
-class HomePage extends StatelessWidget {
-  HomePage({super.key});
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
 
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final user = FirebaseAuth.instance.currentUser!;
-  
+
   // sign user out method
   void signUserOut() {
     FirebaseAuth.instance.signOut();
   }
 
-  //create a function loading and returning the user's projects from firestore
   Future<List<QueryDocumentSnapshot<Map<String, dynamic>>>> loadUserProjects() async {
-    final userProjects = await FirebaseFirestore.instance
-        .collection('aiditor_projects')
-        .where('user_uid', isEqualTo: user.uid)
-        .get();
-    return userProjects.docs;
-  }
+  final querySnapshot = await FirebaseFirestore.instance
+      .collection('aiditor_projects').where(
+        'user_id', isEqualTo: user.uid,
+      ).get();
 
-  
+  final userProjects = querySnapshot.docs;
+  print('Number of user projects: ${userProjects.length}');
+  print('User Projects: $userProjects');
+
+  return userProjects;
+}
+
+
+
+  Future<void> createProject(BuildContext context) async {
+  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  String projectName = '';
+  String fileName = '';
+  String projectType = 'lol';
+
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text('Create Project'),
+        content: Form(
+          key: formKey,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Project Name',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a project name';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  projectName = value!;
+                },
+              ),
+              const SizedBox(height: 16),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'File Name',
+                ),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a file name';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  fileName = value!;
+                },
+              ),
+              const SizedBox(height: 16),
+              DropdownButtonFormField<String>(
+                decoration: const InputDecoration(
+                  labelText: 'Type',
+                ),
+                value: projectType,
+                items: [
+                  'lol',
+                  'lifestyle',
+                ].map((String item) {
+                  return DropdownMenuItem<String>(
+                    value: item,
+                    child: Text(item),
+                  );
+                }).toList(),
+                onChanged: (value) {
+                  setState(() {
+                    projectType = value!;
+                  });
+                },
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please select a project type';
+                  }
+                  return null;
+                },
+              ),
+            ],
+          ),
+        ),
+        actions: [
+          ElevatedButton(
+            onPressed: () async {
+              if (formKey.currentState!.validate()) {
+                formKey.currentState!.save();
+
+                final projectId = DateTime.now().microsecondsSinceEpoch.toString().substring(0, 8);
+                final projectDocId = FirebaseFirestore.instance.collection('aiditor_projects').doc().id;
+
+                final projectData = {
+                  'project_name': projectName,
+                  'created_date': DateFormat('dd-MM-yyyy').format(DateTime.now()),
+                  'user_id': user.uid,
+                  'project_id': projectId,
+                  'project_type': projectType, // Add projectType to projectData
+                };
+
+                final videoData = {
+                  'video_filename': fileName,
+                  'type': projectType,
+                };
+
+                try {
+                  await FirebaseFirestore.instance
+                      .collection('aiditor_projects')
+                      .doc(projectDocId)
+                      .set(projectData);
+
+                  await FirebaseFirestore.instance
+                      .collection('aiditor_projects')
+                      .doc(projectDocId)
+                      .collection('aidited_videos')
+                      .doc()
+                      .set(videoData);
+
+
+                  // Send request to backend
+                  
+                  // delete space & special characters from filename
+                  final fileNameWithoutSpaces = fileName.replaceAll(' ', '_');
+                  final fileNameWithoutSpecialCharacters = fileNameWithoutSpaces.replaceAll(RegExp(r'[^\w\s]+'), '');
+
+                  var date = DateFormat('dd-MM-yyyy').format(DateTime.now());
+
+                  var s3Filename = "$projectId/${fileNameWithoutSpecialCharacters}_${projectType}_$date.mp4";
+
+                  var url = Uri.parse('https://xkr5ezacb2.execute-api.us-east-1.amazonaws.com/v1/url');
+                  var getSignedUrlResponse = await http.get(url);
+
+                  if (getSignedUrlResponse.statusCode == 200) {
+                    var responseBody = json.decode(getSignedUrlResponse.body); // Parse the response body as JSON
+
+                    var signedUrl = responseBody['url'];
+                    var fields = responseBody['fields'];
+
+                    var formData = Map<String, String>.from(fields);
+                    formData['key'] = s3Filename;
+                    formData['file'] = fileName;
+
+                    var postResponse = await http.post(Uri.parse(signedUrl), body: formData);
+
+                    if (postResponse.statusCode == 204) {
+                      // File uploaded successfully
+                      print('File uploaded successfully');
+                    } else {
+                      // File upload failed
+                      print('File upload failed with status code: ${postResponse.statusCode}');
+                    }
+                  } else {
+                    // Failed to get signed URL
+                    print('Failed to get signed URL');
+                  }
+
+                  
+
+                  // Reload projects
+                  setState(() {});
+
+                  Navigator.of(context).pop(); // Close the dialog
+                } catch (error) {
+                  print('Error creating project: $error');
+                  // Handle error
+                }
+              }
+            },
+            child: const Text('Create'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).pop(); // Close the dialog
+            },
+            child: const Text('Cancel'),
+          ),
+        ],
+      );
+    },
+  );
+}
+
 
   @override
   Widget build(BuildContext context) {
@@ -29,18 +217,82 @@ class HomePage extends StatelessWidget {
       backgroundColor: Colors.grey[300],
       appBar: AppBar(
         backgroundColor: Colors.grey[900],
+        centerTitle: true,
+        title: ElevatedButton(
+          onPressed: () => createProject(context),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.blue,
+            foregroundColor: Colors.white,
+          ),
+          child: const Text('Create Project'),
+        ),
         actions: [
           IconButton(
-            onPressed: signUserOut,
+            onPressed: () {
+              // Reload projects
+              setState(() {});
+              signUserOut();
+            },
             icon: const Icon(Icons.logout),
-          )
+          ),
         ],
       ),
-      body: Center(
-          child: Text(
-        "LOGGED IN AS: ${user.email!}\n\nUID: ${user.uid}",
-        style: const TextStyle(fontSize: 20),
-      )),
+      body: SafeArea(
+        child: Center(
+          child: FutureBuilder<List<QueryDocumentSnapshot<Map<String, dynamic>>>>(
+            future: loadUserProjects(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator();
+              } else if (snapshot.hasError) {
+                return Text('Error: ${snapshot.error}');
+              } else if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+                // Display user's projects as cards
+                return GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4, // Number of columns in the grid
+                    childAspectRatio: 4.0,
+                    crossAxisSpacing: 8.0,
+                    mainAxisSpacing: 8.0,
+                  ),
+                  itemCount: snapshot.data!.length, // Updated line
+                  itemBuilder: (context, index) {
+                    final project = snapshot.data![index].data();
+                    final title = project['project_name'] ?? 'No Project Name';
+                    final creationDate = project['created_date'] ?? 'No Creation Date';
+                    final projectId = project['project_id'] ?? 'No Project ID';
+
+                    return Card(
+                      child: ListTile(
+                        title: Text(title + '                                                         ' + projectId),
+                        subtitle: Text(creationDate),
+                        // Add more project details if needed
+                      ),
+                    );
+                  },
+                );
+
+              } else {
+                // Display "No project" text and create project button
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'No project',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                    const SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: () => createProject(context),
+                      child: const Text('Create a project'),
+                    ),
+                  ],
+                );
+              }
+            },
+          ),
+        ),
+      ),
     );
   }
 }
