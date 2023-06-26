e597849b8ee0e51e03e3329d258e56413bd7df53
WxMAMADIANxW <49515789+WxMAMADIANxW@users.noreply.github.com>
2023-06-26 05:07:18 +0200

    Video Player and card list

--- aiditor_web/lib/pages/project_page.dart
+++ aiditor_web/lib/pages/project_page.dart
@@ -1,33 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:youtube_player_iframe_plus/youtube_player_iframe_plus.dart';
 
-class ProjectDetailsScreen extends StatelessWidget {
-  final String projectId;
+class ProjectDetailsScreen extends StatefulWidget {
+  final String? projectId;
 
   ProjectDetailsScreen({required this.projectId});
 
-  Future<List<DocumentSnapshot<Map<String, dynamic>>>> getProjectVideos() async {
+  @override
+  _ProjectDetailsScreenState createState() => _ProjectDetailsScreenState();
+}
 
-    final docId = await FirebaseFirestore.instance
-        .collection('aiditor_projects').where('project_id', isEqualTo: projectId).get()
-        .then((value) => value.docs.first.id);
+class _ProjectDetailsScreenState extends State<ProjectDetailsScreen> {
+  late YoutubePlayerController _controller;
+  late String _currentVideoUrl;
 
-    final QuerySnapshot<Map<String, dynamic>> querySnapshot = await FirebaseFirestore.instance
-        .collection('aiditor_projects/$docId/aidited_videos')
-        .get();
+  @override
+  void initState() {
+    super.initState();
 
-    final projectVideos = querySnapshot.docs;
-    
-    print('DocId: $docId');
-    print('Number of project videos: ${projectVideos.length}');
-    print('Project videos: $projectVideos');
 
-    return projectVideos;
+    _currentVideoUrl = '';
+    _controller = YoutubePlayerController(
+      initialVideoId: '',
+      params: const YoutubePlayerParams(
+        showControls: true,
+        showFullscreenButton: true,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.close();
+    super.dispose();
+  }
+
+  Widget _buildVideo(String url) {
+    final videoId = YoutubePlayerController.convertUrlToId(url).toString();
+
+    if (videoId != _controller.metadata.videoId) {
+      _controller.load(videoId);
+    }
+
+    return YoutubePlayerIFramePlus(
+      controller: _controller,
+      aspectRatio: 16 / 9,
+    );
   }
 
   void deleteProject() async {
     final docId = await FirebaseFirestore.instance
-        .collection('aiditor_projects').where('project_id', isEqualTo: projectId).get()
+        .collection('aiditor_projects')
+        .where('project_id', isEqualTo: widget.projectId)
+        .get()
         .then((value) => value.docs.first.id);
 
     await FirebaseFirestore.instance
@@ -36,20 +62,94 @@ class ProjectDetailsScreen extends StatelessWidget {
         .delete();
   }
 
+  Future<List<DocumentSnapshot<Map<String, dynamic>>>> getProjectVideos() async {
+    final docId = await FirebaseFirestore.instance
+        .collection('aiditor_projects')
+        .where('project_id', isEqualTo: widget.projectId)
+        .get()
+        .then((value) => value.docs.first.id);
+
+    final QuerySnapshot<Map<String, dynamic>> querySnapshot =
+        await FirebaseFirestore.instance
+            .collection('aiditor_projects/$docId/aidited_videos')
+            .get();
+
+    final projectVideos = querySnapshot.docs;
+
+    print('DocId: $docId');
+    print('Number of project videos: ${projectVideos.length}');
+    print('Project videos: $projectVideos');
+
+    return projectVideos;
+  }
+
   @override
   Widget build(BuildContext context) {
+     
+
+
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Project Details'),
         actions: [
           IconButton(
-            icon: Icon(Icons.delete),
             onPressed: () {
-              // Handle delete project
               deleteProject();
+              Navigator.of(context).pop();
             },
+            icon: Icon(Icons.delete),
           ),
         ],
+      ),
+      body: FutureBuilder<List<DocumentSnapshot<Map<String, dynamic>>>>(
+        future: getProjectVideos(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final projectVideos = snapshot.data!;
+            return Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    margin: EdgeInsets.all(40),
+                    child: _buildVideo(_currentVideoUrl),
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(
+                    margin: EdgeInsets.all(40),
+                    child: ListView.builder(
+                      itemCount: projectVideos.length,
+                      itemBuilder: (context, index) {
+                        final videoData = projectVideos[index].data();
+                        final videoUrl = videoData?['url'];
+                        final videoName = videoData?['filename'];
+                        return Card(
+                          child: ListTile(
+                            onTap: () {
+                              setState(() {
+                                _currentVideoUrl = videoUrl ?? '';
+                              });
+                            },
+                            title: Text(videoName ?? ''),
+                            subtitle: Text(videoUrl ?? ''),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ],
+            );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
       ),
     );
   }
