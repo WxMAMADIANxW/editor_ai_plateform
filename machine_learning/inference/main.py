a8170476f6913b260883a32eaacf23fa2a4c6d56
Réda Maizate <reda.maizate@affluences.com>
2023-06-26 20:46:30 +0200

    chore: cleaning code

--- machine_learning/inference/main.py
+++ machine_learning/inference/main.py
@@ -1,75 +1,156 @@
-# TODO: Get messages from AWS SQS queue
 import boto3
 import json
 import os
 import time
 import logging
+import redis
+from rediscluster import RedisCluster
+
+print('Starting...')
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
-from redis import Redis
-# TODO: Voir si on en a besoin
-#  from rediscluster import RedisCluster
-
-print('Starting...')
 logger.info('Starting...')
 
 # SQS Environment Variables
-# SQS_ENDPOINT = os.environ.get('SQS_ENDPOINT')
 AWS_REGION = os.environ.get('AWS_REGION', 'us-east-1')
+# print(f'AWS region: {AWS_REGION}')
 SQS_QUEUE_NAME = os.environ.get('SQS_QUEUE_NAME')
+# print(f'SQS queue name: {SQS_QUEUE_NAME}')
 
 # Redis Environment Variables
 REDIS_HOST = os.environ.get('REDIS_HOST')
+# print(f'Redis host: {REDIS_HOST}')
 REDIS_PORT = os.environ.get('REDIS_PORT', 6379)
-REDIS_USERNAME = os.environ.get('REDIS_USERNAME')
-REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD')
+# print(f'Redis port: {REDIS_PORT}')
+REDIS_USERNAME = os.environ.get('REDIS_USERNAME', "")
+# print(f'Redis username: {REDIS_USERNAME}')
+REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD', "")
+# print(f'Redis password: {REDIS_PASSWORD}')
 
 # Get the service resource
 sqs = boto3.resource('sqs', region_name=AWS_REGION)
 
 # Get the queue
 queue = sqs.get_queue_by_name(QueueName=SQS_QUEUE_NAME)
-print(f'Queue URL: {queue.url}')
-logger.info(f'Queue URL: {queue.url}')
 
 # Process messages by printing out body and optional author name
-timeout = time.time() + 60*3
+timeout = time.time() + 60 * 3
 
 while True:
     if time.time() > timeout:
         break
     try:
         messages = queue.receive_messages(MessageAttributeNames=['All'], MaxNumberOfMessages=1, WaitTimeSeconds=5)
+        counter = 0
         for message in messages:
-            # Print out the body and author (if set)
-            print('Hello, {0}!'.format(message.body))
-            logger.info('Hello, {0}!'.format(message.body))
-            # Let the queue know that the message is processed
+            print('Consuming a message...')
+            logger.info('Consuming a message...')
+            # print("--------------------")
+            # logger.info("--------------------")
+            payload = json.loads(message.body)
             message.delete()
 
+            if payload.get('Event', None) is not None:
+                print('Test event message received')
+                logger.info('Test event message received')
+                continue
+
+            print(f'Payload: {payload}')
+            logger.info(f'Payload: {payload}')
+            bucket_name = payload.get('Records')[0].get('s3').get('bucket').get('name')
+            print(f'Bucket name: {bucket_name}')
+            logger.info(f'Bucket name: {bucket_name}')
+            object_key = payload.get('Records')[0].get('s3').get('object').get('key')
+            print(f'Object key: {object_key}')
+            logger.info(f'Object key: {object_key}')
+
             # TODO #1: Download videos from S3 bucket to local storage
+            s3_client = boto3.client('s3', region_name=AWS_REGION)
+            file_name = object_key.split('/')[-1]
+
+            try:
+                s3_client.download_file(bucket_name, object_key, file_name)
+            except Exception as e:
+                print("Error during download")
+                logger.error("Error during download")
+                print(e)
+                logger.error(e)
+
+            print(f'Downloaded {object_key} from {bucket_name} to {file_name}')
+            logger.info(f'Downloaded {object_key} from {bucket_name} to {file_name}')
+            # print(f"Local files: {os.listdir()}")
+            # logger.info(f'Local files: {os.listdir()}')
+
             # TODO #2: Run inference
+
+            # TODO #5: Delete videos from S3 bucket and local storage
+            try:
+                os.remove(file_name)
+            except Exception as e:
+                print("Error during local deletion")
+                logger.error("Error during local deletion")
+                print(e)
+                logger.error(e)
+
+            try:
+                s3_client.delete_object(Bucket=bucket_name, Key=object_key)
+            except Exception as e:
+                print("Error during S3 deletion")
+                logger.error("Error during S3 deletion")
+                print(e)
+                logger.error(e)
+            print(f'Deleted {object_key} from {bucket_name} and {file_name}')
+            logger.info(f'Deleted {object_key} from {bucket_name} and {file_name}')
+
             # TODO #3: Push data to AWS ElastiCache (Redis) instance
-            redis = Redis(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True, ssl=True, username=REDIS_USERNAME,
-                          password=REDIS_PASSWORD) # ERROR during connection: Error 110 connecting to editor-ai-platform-elasticache-cluster.tprabp.0001.use1.cache.amazonaws.com:6379. Connection timed out.
+            # logger.info("REDIS_HOST: " + REDIS_HOST)
+            # print("REDIS_HOST: ", REDIS_HOST)
+            # logger.info("REDIS_PORT: " + REDIS_PORT)
+            # print("REDIS_PORT: ", REDIS_PORT)
+            # logger.info("REDIS_USERNAME: " + REDIS_USERNAME)
+            # print("REDIS_USERNAME: ", REDIS_USERNAME)
+            # logger.info("REDIS_PASSWORD: " + REDIS_PASSWORD)
+            # print("REDIS_PASSWORD: ", REDIS_PASSWORD)
+
+            redis_cluster = RedisCluster(host=REDIS_HOST, port=REDIS_PORT, username=REDIS_USERNAME,
+                                         password=REDIS_PASSWORD, decode_responses=True, skip_full_coverage_check=True,
+                                         ssl=True)
             # TODO: Test avec RedisCluster bg
-            if redis.ping():
+            if redis_cluster.ping():
                 print('Connected to Redis')
                 logger.info('Connected to Redis')
             else:
                 print('Could not connect to Redis')
                 logger.info('Could not connect to Redis')
+
             # TODO #4: Push data to AWS ElastiCache (Redis) cluster
-            redis.set('foo', 'bar')
-            print("foo: ", redis.get('foo'))
-            logger.info(f"foo: {redis.get('foo')}")
-            # redis = RedisCluster(host=REDIS_HOST, port=REDIS_PORT, decode_responses=True, ssl=True, username=REDIS_USERNAME,
-            #                      password=REDIS_PASSWORD)
+            # Push list of elements in the key 'foo'
+            key = f'project_id:{counter}'
+            redis_cluster.delete(key)
+
+            d = {"query": "test", "result": "test"}
+            for k, v in d.items():
+                redis_cluster.hset(key, k, v)
+            # redis_cluster.hset('foo', mapping={"query": "test", "result": "test"})
+            print("Pushed data to Redis")
+            logger.info("Pushed data to Redis")
+            # Get the list of elements in the key 'foo'
+
+            values_from_my_key = redis_cluster.hgetall(key)
+            for k, v in values_from_my_key.items():
+                print(k, v)
+                logger.info(k, v)
+
+            redis_cluster.delete(key)  # TODO: Temporary delete for testing purposes
+            counter += 1
+            print("Message consumed")
+            logger.info("Message consumed")
+
     except Exception as e:
-        logger.error(e)
-        print(e)
+        logger.error(f"Error in message consuming: {e}")
+        print(f"Error in message consuming: {e}")
         continue
 
 print('Done getting messages from SQS queue')
